5fe8e824bbee0746abcd4d533cd9d197033ac934
André Abramé <andre.abrame@logike.fr>
2024-02-29 14:13:29 +0100

    generic service in front

--- poll-frontend-angular/src/app/app-routing.module.ts
+++ poll-frontend-angular/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { PollsComponent } from './components/polls/polls.component';
 import { Polls2Component } from './components/polls2/polls2.component';
+import { isAnonymousGuard } from './guards/is-anonymous.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent, canActivate: [isAnonymousGuard]},
   { path: 'polls2', component: Polls2Component },
   { path: '', component: PollsComponent }
 ];
--- poll-frontend-angular/src/app/services/poll.service.ts
+++ poll-frontend-angular/src/app/services/poll.service.ts
@@ -3,34 +3,20 @@ import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable, delay } from 'rxjs';
 import { Poll } from '../models/poll.model';
+import { GenericService } from './generic.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PollService {
+export class PollService extends GenericService<Poll> {
 
 
-  private url = environment.backendUrl + 'polls';
+  constructor(httpClient: HttpClient) {
+    super(httpClient, environment.backendUrl + "polls");
+   }
 
-  constructor(private httpClient: HttpClient) { }
-
-  findAll(q?: string): Observable<Poll[]> {
+  override findAll(q?: string): Observable<Poll[]> {
     return this.httpClient.get<Poll[]>(this.url, q ? { params: { q: q} } : undefined).pipe(delay(1000));
   }
 
-  findById(id: number): Observable<Poll> {
-    return this.httpClient.get<Poll>(this.url + "/" + id).pipe(delay(1000));
-  }
-
-  save(poll: Partial<Poll>): Observable<Poll> {
-    return this.httpClient.post<Poll>(this.url, poll).pipe(delay(1000));
-  }
-
-  update(poll: Partial<Poll>): Observable<Poll> {
-    return this.httpClient.put<Poll>(this.url + "/" + poll.id, poll).pipe(delay(1000));
-  }
-
-  deleteById(id: number): Observable<void> {
-    return this.httpClient.delete<void>(this.url + "/" + id).pipe(delay(1000));
-  }
 }
